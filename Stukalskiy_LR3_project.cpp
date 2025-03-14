64e640a75216d3549177b1c0ad193e0333088dfe
legkosdelal <vladosmaso@gmail.com>
2025-03-14 11:49:51 +0300

    Create void main

---
+++ Stukalskiy_LR3_project.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+void EnterNumberA()
+{
+
+}
+
+void EnterNumberB()
+{
+
+}
+
+void EnterNumberC()
+{
+
+}
+
+void SuchTreug()
+{
+
+}
+
+void PTreug()
+{
+
+}
+
+int main()
+{
+int i = 6;
+
+    switch (i)
+    {
+    case 1:
+        EnterNumberA();
+        break;
+    case 2:
+        EnterNumberB();
+        break;
+    case 3:
+        EnterNumberC();
+        break;
+    case 4:
+        SuchTreug();
+        break;
+    case 5:
+        PTreug();
+        break;
+    case 6:
+        cout <<"Thank you for playing!\n";
+        break;
+    }
+    
+}
