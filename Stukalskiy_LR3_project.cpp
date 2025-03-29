e5bfa9d7720377bd9c08ecc8aed3971b25d0c778
legkosdelal <vladosmaso@gmail.com>
2025-03-29 15:20:52 +0300

    third

--- Stukalskiy_LR3_project.cpp
+++ Stukalskiy_LR3_project.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 using namespace std;
 
+int a, b, c;
+
 void EnterNumbers()
 {
-
+    cout << "Введите длину стороны A: ";
+    cin >> a;
+    cout << "Введите длину стороны B: ";
+    cin >> b;
+    cout << "Введите длину стороны C: ";
+    cin >> c;
 }
 
 void SuchTreug()
@@ -16,24 +23,35 @@ void PTreug()
 
 }
 
-int main()
-{
-int i = 6;
-
-    switch (i)
-    {
-    case 1:
-        EnterNumbers();
-        break;
-    case 2:
-        SuchTreug();
-        break;
-    case 3:
-        PTreug();
-        break;
-    case 4:
-        cout <<"Thank you for playing!\n";
-        break;
-    }
+int main() {
+    int choice;
     
+    do {
+        cout << "\nМеню:" << endl;
+        cout << "1. Ввести длины сторон" << endl;
+        cout << "2. Проверить существование треугольника" << endl;
+        cout << "3. Вычислить периметр" << endl;
+        cout << "4. Выход" << endl;
+        cout << "Выберите действие: ";
+        cin >> choice;
+        
+        switch (choice) {
+            case 1:
+                EnterNumbers();
+                break;
+            case 2:
+                SuchTreug();
+                break;
+            case 3:
+                PTreug();
+                break;
+            case 4:
+                cout << "Спасибо за использование программы!" << endl;
+                break;
+            default:
+                cout << "Неверный выбор. Попробуйте снова." << endl;
+        }
+    } while (choice != 4);
+    
+    return 0;
 }
